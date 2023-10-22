4455fe7a48ba43a0e3da40a2b9348c61fcaa0b69
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-10-22 23:49:18 +0300

    clean template
    +add favicon

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../images/logo.jpg';
+
+const Logo = () => {
+    
+  return (
+    <div>
+      <img src={logo} alt="logo" />
+    </div>
+  );
+};
+
+export default Logo;
