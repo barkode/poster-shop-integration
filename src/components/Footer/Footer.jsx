eb4b7f0c385593b9f8bbd717a45072cde7c312e5
barkode <konstantinks@gmail.com>
2023-11-05 01:30:39 +0000

    test footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,42 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { getColorTheme } from 'redux/theme/selector';
-import { getAllProducts } from 'redux/products/selector';
+import { setLanguage } from 'redux/language/actions';
+import { getAllProducts } from 'redux/data/selector';
 import { getLanguage } from 'redux/language/selectors';
+import { setColorTheme } from 'redux/theme/actions';
 
 const Footer = () => {
   const colorTheme = useSelector(getColorTheme);
-  console.log({ colorTheme });
+  // console.log({ colorTheme });
 
   const prod = useSelector(getAllProducts);
-  console.log({ products: prod });
+  // console.log({ products: prod });
 
   const lang = useSelector(getLanguage);
-  console.log({ Language: lang });
-  return <> Footer </>;
+  // console.log({ Language: lang });
+
+  const dispatch = useDispatch();
+
+  const handleColorSwitch = () => {
+    // console.log('handleColor Switch is executed.');
+    dispatch(setColorTheme(colorTheme === 'light' ? 'dark' : 'light'));
+  };
+  const handleLanguageSwitch = () => {
+    // console.log('handleLanguage Switch is executed.');
+    dispatch(setLanguage(lang === 'ua' ? 'en' : 'ua'));
+  };
+
+  return (
+    <>
+      <button type="button" onClick={handleColorSwitch}>
+        COLOR BUTTON
+      </button>
+      <button type="button" onClick={handleLanguageSwitch}>
+        LANGUAGE BUTTON
+      </button>
+    </>
+  );
 };
 
 export default Footer;
