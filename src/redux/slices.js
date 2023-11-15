3a3fcaa1f42f3fa40c979fd8794caab4c3ed991b
barkode <konstantinks@gmail.com>
2023-11-15 23:55:38 +0000

    Update slices.js

--- src/redux/slices.js
+++ src/redux/slices.js
@@ -1,6 +1,6 @@
 export * from 'redux/theme/sliceTheme';
 export * from 'redux/language/sliceLanguage';
 export * from 'redux/products/sliceProducts';
+export * from './favorite/sliceFavorites';
+export * from './basket/sliceBasket';
 // export * from './auth/slice';
-// export * from './basket/slice';
-// export * from './favorite/slice';
