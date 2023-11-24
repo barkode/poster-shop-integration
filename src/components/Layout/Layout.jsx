186bfff75f35cb46b434b7e24b37038f16bba87c
barkode <konstantinks@gmail.com>
2023-11-24 02:13:14 +0000

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,7 +2,6 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Container } from './Layout.styled';
 import Header from 'components/Header/Header';
-import HeaderTop from 'components/HeaderTop/HeaderTop';
 import Footer from 'components/Footer/Footer';
 
 const Layout = () => {
